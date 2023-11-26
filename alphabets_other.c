e3ce59a99b7a2f2531abb85ff0606eae91185c68
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2023-11-26 12:51:06 +0530

    Alphabets printing code

---
+++ alphabets_other.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	int n,i;
+	
+	printf("Alphabets are as follows :\n");
+	
+	for(i=65;i<=90;i++)
+	{
+		printf(" %c",i);
+	}
+	printf("\n");
+	for(i=97;i<=122;i++)
+	{
+		printf(" %c",i);
+	}
+}
