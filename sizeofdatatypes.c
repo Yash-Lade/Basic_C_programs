964d36801c1e80b4735e0a3de4cda5d067d640df
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2023-11-27 21:38:16 +0530

    Update and rename sizeofdatatypes.cpp to sizeofdatatypes.c

--- sizeofdatatypes.c
+++ sizeofdatatypes.c
@@ -3,6 +3,8 @@
 int main()
 {
 	int i;float f;double d;char c;
+
+	printf("@Author- Yash Lade \n");
 	
 	printf("\nThe value of int i is %ld",sizeof(i));
 	printf("\nThe value of float f is %ld",sizeof(f));
