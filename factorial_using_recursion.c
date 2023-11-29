8d3bc50563d8b50320c70112a83c240e9ada1927
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2023-11-29 23:08:58 +0530

    Program to find factorial of a number using recusion
    This program calculates the factorial of the entered number .And this is achieved with the help of recursion.

---
+++ factorial_using_recursion.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int fact(int);
+
+int main()
+{
+	int n,b;
+	
+	printf("Enter the number :");
+	scanf("%d",&n);
+	
+	b=fact(n);
+	
+	printf("The factorial of %d is : %d",n,b);
+}
+
+int fact(n)
+{
+	if (n==1)
+	return 1;
+	
+	else
+	return n*fact(n-1);
+}
